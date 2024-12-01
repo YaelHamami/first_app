a6f71978075fb1d9f0f78c8c2f3ffbe98369f859
Your Name <you@example.com>
2024-12-01 23:06:28 +0200

    remove unnecessary try catch

--- src/controllers/posts_controller.ts
+++ src/controllers/posts_controller.ts
@@ -1,19 +1,15 @@
 import { postModel } from "../models/posts_model";
 
 export const getPostById = async (req: any, res: any) => {
+    const postId = req.params.id;
     try {
-        const postId = req.params.id;
-        try {
-            const post = await postModel.findById(postId);
-            if (post != null) {
-                res.status(200).send(post);
-              } else {
-                res.status(404).send("Post not found");
-              }
-        } catch (error: any) {
-            res.status(400).send(error.message);
-        }
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
+        const post = await postModel.findById(postId);
+        if (post != null) {
+            res.status(200).send(post);
+            } else {
+            res.status(404).send("Post not found");
+            }
+    } catch (error: any) {
+        res.status(400).send(error.message);
     }
 };
