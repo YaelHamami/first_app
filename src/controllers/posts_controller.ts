63c06f06910a88f165176e606efab9c57183c30d
sagiezra <sagiezra0@gmail.com>
2024-12-01 21:56:00 +0200

    update post flow added

--- src/controllers/posts_controller.ts
+++ src/controllers/posts_controller.ts
@@ -1,11 +1,21 @@
 import { postModel } from "../models/posts_model";
 
 export const createPost = async (req: any, res: any) => {
-  const postBody = req.body;
-  try {
-    const post = await postModel.create(postBody);
-    res.status(201).send(post);
-  } catch (error: any) {
-    res.status(400).send(error.message);
-  }
+    const postBody = req.body;
+    try {
+        const post = await postModel.create(postBody);
+        res.status(201).send(post);
+    } catch (error: any) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const updatePost = async (req: any, res: any) => {
+    try {
+        const updatedPost = await postModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedPost) return res.status(404).json({ message: 'Post not found' });
+        res.status(200).json(updatedPost);
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
 };
