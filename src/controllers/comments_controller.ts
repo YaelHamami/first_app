583d5e20102944dafeb96faf63fb35abcb92080b
Your Name <you@example.com>
2024-12-02 21:04:13 +0200

    add comments model and createposts controller logic

--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -1,3 +1,11 @@
+import { commentModel } from "../models/comments_model";
+
 export const createComment = async (req: any, res: any) => {
-    res.send("create a comment");  
+    const commentBody = req.body;
+    try {
+        const post = await commentModel.create(commentBody);
+        res.status(201).send(post);
+    } catch (error: any) {
+        res.status(400).send(error.message);
+    }
 };
