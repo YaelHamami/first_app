f794600fe03f4ea464b4a2c74439863f20a58ead
Your Name <you@example.com>
2024-12-03 17:24:37 +0200

    fix comment

--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import { createComment } from "../controllers/comments_controller";
 export const commentRoutes = express.Router();
 
-// Create Post
+// Create Comment
 commentRoutes.post("/", createComment);
