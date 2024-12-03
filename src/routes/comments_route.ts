461cdc1b035e19778400d5376014b3948825ef5b
sagiezra <sagiezra0@gmail.com>
2024-12-03 23:30:33 +0200

    added get comment by id api

--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createComment } from "../controllers/comments_controller";
+import { createComment, getCommentById } from "../controllers/comments_controller";
 import { getAllComments } from '../controllers/comments_controller';
 export const commentsRouter = express.Router();
 
 // Get All Comments
 commentsRouter.get('/', getAllComments);
+
+// Get Post By Id
+commentsRouter.get('/:id', getCommentById);
 
 // Create Comment
 commentsRouter.post("/", createComment);
