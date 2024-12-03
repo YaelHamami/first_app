96df8cd9d13c806ac7c791222c25d5bcce6005cf
sagiezra <sagiezra0@gmail.com>
2024-12-03 22:07:11 +0200

    removed unrelated stuff

--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -10,37 +10,6 @@ export const getAllComments = async (req: any, res: any) => {
     }
 };
 
-// Get Comment By Id
-export const getCommentById = async (req: any, res: any) => {
-    const commentId = req.params.id;
-    try {
-        const comment = await commentModel.findById(commentId);
-        if (comment != null) {
-            res.status(200).send(comment);
-        } else {
-            res.status(404).send("Comment not found");
-        }
-    } catch (error: any) {
-        res.status(400).send(error.message);
-    }
-};
-
-// Get Comments By PostId
-export const getCommentsByPostId = async (req: any, res: any) => {
-    const postId = req.params.postId;
-
-    try {
-        const comments = await commentModel.find({ postId });
-        if (comments != null) {
-            res.status(200).send(comments);
-        } else {
-            res.status(404).send(`Comments of postId: ${postId} ,were not found`);
-        }
-    } catch (error: any) {
-        res.status(400).send(error.message);
-    }
-};
-
 // Create Comment
 export const createComment = async (req: any, res: any) => {
     const commentBody = req.body;
--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -1,16 +1,10 @@
 import express from 'express';
-import { createComment, getCommentsByPostId } from "../controllers/comments_controller";
-import { getAllComments, getCommentById } from '../controllers/comments_controller';
+import { createComment } from "../controllers/comments_controller";
+import { getAllComments } from '../controllers/comments_controller';
 export const commentsRouter = express.Router();
 
 // Get All Comments
 commentsRouter.get('/', getAllComments);
-
-// Get Post By Id
-commentsRouter.get('/:id', getCommentById);
-
-// Get Post By postId
-commentsRouter.get('/:postId', getCommentsByPostId);
 
 // Create Comment
 commentsRouter.post("/", createComment);
