e8985ecbe3ba2ab7fdfcb0d1dc6f27c378b7d22f
Your Name <you@example.com>
2024-12-04 21:30:43 +0200

    add update commit api

--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getAllComments, deleteComment, getCommentById } from "../controllers/comments_controller";
+import { createComment, getAllComments, deleteComment, getCommentById, updateComment } from "../controllers/comments_controller";
 export const commentsRouter = express.Router();
 
 // Get All Comments
@@ -13,3 +13,6 @@ commentsRouter.post("/", createComment);
 
 // Delete Comment By ID
 commentsRouter.delete('/:id', deleteComment);
+
+// Update Comment By ID
+commentsRouter.put('/:id', updateComment);
