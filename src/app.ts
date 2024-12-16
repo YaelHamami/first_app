df534d2d9a95729c739de5f78f2ac311ebcdd1d0
sagiezra <sagiezra0@gmail.com>
2024-12-16 18:20:57 +0200

    added auth middleware login and register

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import { postsRouter } from './routes/posts_route';
 import { commentsRouter } from './routes/comments_route';
 import local_mongoose from "mongoose";
+import { authRouter } from './routes/auth_route';
 
 const app = express();
 dotenv.config();
@@ -18,6 +19,7 @@ db.once("open", () => console.log("Connected to Database"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use('/auth', authRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
 
