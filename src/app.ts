bdb0424b53544608557af2ea01e6093054f2445e
sagiezra <sagiezra0@gmail.com>
2024-12-22 13:25:38 +0200

    added initApp function for tests

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,8 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from "dotenv";
-import { postsRouter } from './routes/posts_route';
-import { commentsRouter } from './routes/comments_route';
-import local_mongoose from "mongoose";
-import { authRouter } from './routes/auth_route';
-
-const app = express();
-dotenv.config();
+import { initApp } from "./server";
 const port = process.env.PORT;
 
-local_mongoose.connect(process.env.DB_CONNECTION);
-const db = mongoose.connection;
-db.on("error", (error: Error) => console.error(error));
-db.once("open", () => console.log("Connected to Database"));
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}));
-
-app.use('/auth', authRouter);
-app.use('/posts', postsRouter);
-app.use('/comments', commentsRouter);
-
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+initApp().then((app) => {
+    app.listen(port, () => {
+        console.log(`Server listening at http://localhost:${port}`);
+    });
 });
---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from "dotenv";
+import { postsRouter } from './routes/posts_route';
+import { commentsRouter } from './routes/comments_route';
+import local_mongoose from "mongoose";
+import { authRouter } from './routes/auth_route';
+
+dotenv.config();
+const port = process.env.PORT;
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+app.use('/auth', authRouter);
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+
+const db = mongoose.connection;
+db.on("error", (error: Error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+export const initApp = () => {
+    return new Promise<any>((resolve, reject) => {
+        if (!process.env.DB_CONNECTION) {
+            reject("DB_CONNECTION is not defined in .env file");
+        } else {
+            local_mongoose
+            .connect(process.env.DB_CONNECTION)
+            .then(() => {
+                    resolve(app);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        }
+    });
+};
