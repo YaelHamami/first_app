19c3a853669b59aa4f7009a54bcc04300007f8da
sagiezra <sagiezra0@gmail.com>
2024-12-22 13:12:49 +0200

    logout and refreshtoken

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import userModel from '../models/users_model';
+import userModel, { IUser } from '../models/users_model';
+import { Document } from 'mongoose';
 
 export const register = async (req: Request, res: Response) => {
     try {
@@ -17,6 +18,35 @@ export const register = async (req: Request, res: Response) => {
     } catch (err) {
         res.status(400).send(err);
     }
+};
+
+type tTokens = {
+    accessToken: string,
+    refreshToken: string
+}
+const generateToken = (userId: string): tTokens | null => {
+    if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET) {
+        return null;
+    }
+    // generate token
+    const random = Math.random().toString();
+    const accessToken = jwt.sign({
+        _id: userId,
+        random: random
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.JWT_TOKEN_EXPIRATION });
+
+    const refreshToken = jwt.sign({
+        _id: userId,
+        random: random
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.JWT_TOKEN_EXPIRATION });
+    return {
+        accessToken: accessToken,
+        refreshToken: refreshToken
+    };
 };
 
 export const login = async (req: Request, res: Response) => {
@@ -35,14 +65,116 @@ export const login = async (req: Request, res: Response) => {
             res.status(500).send('Server Error');
             return;
         }
-        // generate access token
-        const accessToken = jwt.sign({ _id: user._id },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: process.env.JWT_TOKEN_EXPIRATION });
-        res.status(200).send({ accessToken, _id: user._id });
+        // generate tokens
+        const tokens = generateToken(user._id);
+        if (!tokens) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        if (!user.refreshToken) {
+            user.refreshToken = [tokens.refreshToken];
+        } else {
+            user.refreshToken.push(tokens.refreshToken);
+        }
+
+        await user.save()
+        res.status(200).send({ 
+            accessToken: tokens.accessToken,
+            refreshToken: tokens.refreshToken,
+            _id: user._id 
+        });
     } catch (err) {
         console.log('error: ', err)
         res.status(400).send(err);
+    }
+};
+
+type tUser = Document<unknown, {}, IUser> & IUser & Required<{
+    _id: string;
+}> & {
+    __v: number;
+}
+const verifyRefreshToken = (refreshToken: string | undefined) => {
+    return new Promise<tUser>((resolve, reject) => {
+        //get refresh token from body
+        if (!refreshToken) {
+            reject("fail");
+            return;
+        }
+        //verify token
+        if (!process.env.TOKEN_SECRET) {
+            reject("fail");
+            return;
+        }
+        jwt.verify(refreshToken, process.env.TOKEN_SECRET, async (err: any, payload: any) => {
+            if (err) {
+                reject("fail");
+                return
+            }
+            //get the user id fromn token
+            const userId = payload._id;
+            try {
+                //get the user form the db
+                const user = await userModel.findById(userId);
+                if (!user) {
+                    reject("fail");
+                    return;
+                }
+                if (!user.refreshToken || !user.refreshToken.includes(refreshToken)) {
+                    user.refreshToken = [];
+                    await user.save();
+                    reject("fail");
+                    return;
+                }
+                const tokens = user.refreshToken!.filter((token) => token !== refreshToken);
+                user.refreshToken = tokens;
+
+                resolve(user);
+            } catch (err) {
+                reject("fail");
+                return;
+            }
+        });
+    });
+}
+
+export const refreshToken = async (req: Request, res: Response) => {
+    try {
+        const user = await verifyRefreshToken(req.body.refreshToken);
+        if (!user) {
+            res.status(400).send("fail");
+            return;
+        }
+        const tokens = generateToken(user._id);
+
+        if (!tokens) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        if (!user.refreshToken) {
+            user.refreshToken = [];
+        }
+        user.refreshToken.push(tokens.refreshToken);
+        await user.save();
+        res.status(200).send(
+            {
+                accessToken: tokens.accessToken,
+                refreshToken: tokens.refreshToken,
+                _id: user._id
+            });
+        //send new token
+    } catch (err) {
+        res.status(400).send("fail");
+    }
+};
+
+export const logout = async (req: Request, res: Response) => {
+    try {
+        const user = await verifyRefreshToken(req.body.refreshToken);
+        await user.save();
+        res.status(200).send("success");
+    } catch (err) {
+        res.status(400).send("fail");
     }
 };
 
@@ -51,7 +183,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const token = authorization && authorization.split(' ')[1];
 
     if (!token) {
-        res.status(401).send('Access Denied');
+        res.status(401).send('Unauthorized');
         return;
     }
     if (!process.env.ACCESS_TOKEN_SECRET) {
---
+++ src/tests/auth.test.ts
@@ -0,0 +1,7 @@
+import { request } from "http"
+
+describe("Restric access without Auth / ", () => {
+    test("getAllPosts. should respond with error", async () => {
+        // const res = await request('').get("/post")
+    })
+})
