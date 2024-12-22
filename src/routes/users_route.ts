4029dc87043912c0a52f59aa2c6b7c63ac14f514
Your Name <you@example.com>
2024-12-22 17:13:10 +0200

    add get all reout

--- src/routes/users_route.ts
+++ src/routes/users_route.ts
@@ -6,3 +6,5 @@ export const usersRouter = express.Router();
 // Delete User By ID
 usersRouter.delete('/:id', authMiddleware, usersController.delete.bind(usersController));
 
+// Get All Posts, or all posts published by a sender if provided
+usersRouter.get('/', authMiddleware, usersController.getAll.bind(usersController));
